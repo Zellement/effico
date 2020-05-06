c82341329db99f3eebdc645cda74518d1f9b6a07
[Dan Farrow] <dan@zellement.com>
2020-05-06 17:10:16 +0100

    LOgo component & header change

--- src/components/header.js
+++ src/components/header.js
@@ -1,41 +1,17 @@
 import React from "react"
-import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import { motion } from 'framer-motion'
 import Nav from '../components/nav'
+import Logo from '../components/atoms/logo'
 
-const Header = ({ siteTitle }) => (
-  <header className="py-8 md:py-16">
-    
-    <div className="container">
-      <div className="flex flex-wrap items-center mb-4">
-        <motion.button
-          className="w-5 h-5 mr-3 block bg-black rounded"
-          animate={{ rotate: 180 }}
-          transition={{
-            loop: Infinity,
-            duration: 2,
-            ease: "anticipate"
-          }}
-        />
+const Header = () => (
+  <header className="">
 
-        <Link className="font-bold block text-lg md:text-xl inline-block" to="/">
-          {siteTitle}
-        </Link>
-      </div>
-
-      <Nav />
-
+    <div className="flex p-4">
+      <Logo className="block w-40 mx-auto" />
     </div>
+
+    <Nav />
+
   </header>
 )
-
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
-}
 
 export default Header
