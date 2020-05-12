ccf91db8b4d4ce8950dd0dddbafe303d03892589
Dan Farrow <dan@zellement.com>
2020-05-12 20:12:20 +0100

    404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,55 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { motion } from "framer-motion"
+import { Link } from "gatsby"
+import Img from "gatsby-image"
+// import Img from 'gatsby-image'
+
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </>
-)
+const duration = 0.35
 
-export default NotFoundPage
+const container = {
+  visible: {
+    transition: {
+      when: "beforeChildren",
+      staggerChildren: 0.2,
+      delayChildren: duration,
+    },
+  },
+}
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1,
+  },
+}
+
+const ServicePage = () => {
+
+  return (
+    <>
+      <SEO title="Recent Projects" description="See our recent projects" />
+
+      <motion.section
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        className=""
+      >
+        <motion.div
+          variants={item}
+          transition="easeInOut"
+          className="container w-full"
+        >
+          <h1>Sorry!</h1>
+
+          <p>This page doesn't exist. Please go to the <Link className="underline" to="/">home page</Link>.</p>
+        </motion.div>
+      </motion.section>
+    </>
+  )
+}
+
+export default ServicePage
